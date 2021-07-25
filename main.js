1f1eaa9c40b8503c6026f57ae798e520d9a259a3
samarthkeshari <samarth.keshari@gmail.com>
2021-07-25 14:45:55 -0700

    test  commit

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import {addHtml,showOverviewChart} from './overview.js'
 
-var data= await d3.csv('./data/time_series_covid19_confirmed_US.csv');
-//var data= await d3.csv('https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv');
+
+async function getData() {
+    data = await(d3.csv('./data/time_series_covid19_confirmed_US.csv'));
+    return data;
+    //console.log(data);
+};
+
+var data = getData();
 
 function overview() {
     //addHtml();
