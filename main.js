bfa8e812adbcaa1e3fd3e95476ba729feaf40da0
samarthkeshari <samarth.keshari@gmail.com>
2021-07-25 15:31:30 -0700

    test-commit-csvread

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import {addHtml,showOverviewChart} from './overview.js'
 
 
 async function getData() {
-    data = await(d3.csv('./data/time_series_covid19_confirmed_US.csv'));
+    data = await(d3.csv('https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'));
     return data;
     //console.log(data);
 };
